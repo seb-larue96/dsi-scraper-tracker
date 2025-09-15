fb8da2d0fce3dc73c31337635bb98138d602de15
seb-larue96 <larue.sebastien@dsi-oi.com>
2025-09-15 12:02:58 +0400

    added swagger open api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import mikroOrmConfig from './config/mikro-orm.config'
+import { HttpModule } from '@nestjs/axios';
+import { OffresModule } from './application/offres/offres.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    MikroOrmModule.forRoot(mikroOrmConfig),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    HttpModule,
+    OffresModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
