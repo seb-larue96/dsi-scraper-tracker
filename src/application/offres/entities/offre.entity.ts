fc63101050aa4bf209c6492ba45845feef0442dd
seb-larue96 <larue.sebastien@dsi-oi.com>
2025-09-15 11:13:00 +0400

    added offre entity

---
+++ src/application/offres/entities/offre.entity.ts
@@ -0,0 +1,28 @@
+import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+
+@Entity()
+export class Offre {
+    @PrimaryKey()
+    id: number;
+  
+    @Property()
+    key: string;
+
+    @Property({ nullable: true })
+    destination?: string;
+
+    @Property({ nullable: true })
+    hotel?: string;
+
+    @Property({ nullable: true })
+    type?: string;
+
+    @Property({ type: 'decimal', precision: 10, scale: 2, nullable: true })
+    prix?: string;
+
+    @Property()
+    urlSite: string;
+
+    @Property({ type: 'date' })
+    scrapedAt: Date;
+}
