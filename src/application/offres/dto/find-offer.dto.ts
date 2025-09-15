8d49e20bd189e3d14728814bf38eedc6512dd8f2
seb-larue96 <larue.sebastien@dsi-oi.com>
2025-09-15 11:53:43 +0400

    added find offer dto

---
+++ src/application/offres/dto/find-offer.dto.ts
@@ -0,0 +1,10 @@
+export class FindOfferDto {
+    id: number;
+    key: string;
+    destination?: string;
+    hotel?: string;
+    type?: string;
+    prix?: string;
+    urlSite: string;
+    scrapedAt: string;
+}
