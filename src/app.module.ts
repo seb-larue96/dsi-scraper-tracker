05358372d36bed04f03e4a502b40838cabe92650
seb-larue96 <larue.sebastien@dsi-oi.com>
2025-09-16 14:52:05 +0400

    updated app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
-import mikroOrmConfig from './config/mikro-orm.config'
+import mikroOrmConfig from './config/mikro-orm.config';
+import { ScheduleModule } from '@nestjs/schedule';
 import { HttpModule } from '@nestjs/axios';
 import { ApplicationModule } from './application/application.module';
 import { LoggerService } from './logger/logger.service';
 import { Log } from './logger/entities/log.entity';
+import { TasksService } from './application/tasks/tasks.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MikroOrmModule.forRoot(mikroOrmConfig),
     MikroOrmModule.forFeature([Log]),
+    ScheduleModule.forRoot(),
     HttpModule,
     ApplicationModule
   ],
-  providers: [LoggerService],
+  providers: [LoggerService, TasksService],
   exports: [LoggerService]
 })
 export class AppModule {}
