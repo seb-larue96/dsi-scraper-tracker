2bcc7fbd724d22494d2dd2fddac1bec1e7a36d40
seb-larue96 <larue.sebastien@dsi-oi.com>
2025-09-17 11:10:30 +0400

    updated main.ts

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
   configureGlobalInterceptors(app);
   configureGlobalFilters(app);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
 }
 bootstrap();
