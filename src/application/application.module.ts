7e34cb7eb63bd0277ea26a9b94b8988ce53dc0cf
seb-larue96 <larue.sebastien@dsi-oi.com>
2025-12-22 10:33:02 +0400

    add offres prix historique module

--- src/application/application.module.ts
+++ src/application/application.module.ts
@@ -3,9 +3,10 @@ import { OffresModule } from "./offres/offres.module";
 import { DestinationsModule } from './destinations/destinations.module';
 import { HotelsModule } from './hotels/hotels.module';
 import { OffresDatesModule } from './offres_dates/offres_dates.module';
+import { OffresPrixHistoriqueModule } from './offres_prix_historique/offres_prix_historique.module';
 
 @Module({
-    imports: [OffresModule, DestinationsModule, HotelsModule, OffresDatesModule],
+    imports: [OffresModule, DestinationsModule, HotelsModule, OffresDatesModule, OffresPrixHistoriqueModule],
     exports: [OffresModule]
 })
 export class ApplicationModule {}
