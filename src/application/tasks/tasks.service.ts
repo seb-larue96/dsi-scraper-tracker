24ca76d156f613d09412dc0a9cc2ce21e4fb67ec
seb-larue96 <larue.sebastien@dsi-oi.com>
2025-12-31 09:56:57 +0400

    update task service

--- src/application/tasks/tasks.service.ts
+++ src/application/tasks/tasks.service.ts
@@ -1,18 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { Cron, CronExpression } from '@nestjs/schedule';
 import { OffresService } from '../offres/offres.service';
 import { LoggerService } from 'src/logger/logger.service';
-import { Cron, CronExpression } from '@nestjs/schedule';
-
+import { FeatureFlags } from 'src/config/feature-flags.config';
 @Injectable()
 export class TasksService {
     private readonly context = TasksService.name;
 
-    constructor(
-        private readonly offresService: OffresService,
-        private readonly logger: LoggerService
-    ) {}
+    constructor(private readonly offresService: OffresService, private readonly logger: LoggerService) {}
 
-    //@Cron(CronExpression.EVERY_DAY_AT_5PM)
+    @Cron(CronExpression.EVERY_HOUR, { disabled: !FeatureFlags.FETCH_AND_STORE_CRON })
     async cronFetchAndStore() {
         await this.logger.info('Starting scheduled fetchAndStore()', this.context);
         try {
